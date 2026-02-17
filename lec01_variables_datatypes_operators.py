b6f819015f7a2c46dbca86ad101d8405b33a783a
malaikaarif <malaikaarif905@gmail.com>
2026-02-18 00:35:36 +0500

    Add Lecture 01: Variables, Data Types, and Operators

---
+++ lec01_variables_datatypes_operators.py
@@ -0,0 +1,68 @@
+# Lecture 01: Variables, Data Types, and Operators
+# Repository: python-for-ai-ml
+# Author: Malaika Arif
+# Description: This file demonstrates basic Python variables, data types, and operators.
+
+# -------------------------------
+# Variables and Data Types
+# -------------------------------
+
+name="Malaika"              # string type
+age=22                      # integer type
+height=5.25                 # float type
+is_student=True             # boolean type
+
+
+# Printing values and their data types
+print("Name:", name)
+print("Name:", type(name))
+
+print(age)
+print(type(age))
+print(height)
+print(type(height))
+print(is_student)
+print(type(is_student))
+
+
+
+
+
+# -------------------------------
+# Arithmetic Operators
+# -------------------------------
+a=10
+b=3
+ 
+print(a+b)          # Addition
+print(a-b)          # Subtraction
+print(a*b)          # Multiplication
+print(a/b)          # Division
+print(a//b)         # Floor Division
+print(a%b)          # Modulo
+print(a**b)         # Power
+
+
+
+
+
+# -------------------------------
+# Comparison Operators
+# -------------------------------
+print("Is a greater than b?", a>b)
+print("Is a equal to b?", a==b)
+
+
+
+
+
+# -------------------------------
+# Comparison Operators
+# -------------------------------
+
+x=True
+y=False
+
+print("x AND y:", x and y)
+print("x OR y:", x or y)
+print("not x :", not x)
